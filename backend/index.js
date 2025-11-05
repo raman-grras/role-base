e675b6549f432bb68e3e47d79ffafcb444eef36a
raman-grras <raman.saini@grrassolutions.in>
2025-11-05 11:08:37 +0530

    update1

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors())
 // body-parser
 app.use(bodyParser.json())
 
-// Serve static files from the React app build directory
+
 app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 app.get('/', (req, res)=>{
@@ -27,7 +27,7 @@ const userRouter = require('./routes/userRoute')
 
 app.use('/users', userRouter)
 
-// Catch all handler: send back React's index.html file for any non-API routes
+
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
